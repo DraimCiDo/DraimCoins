f7c6e96c9ce4a13732a80c9c5ff93dee254b92cd
DraimGooSe <danilaorlov4@gmail.com>
2021-11-23 11:25:53 +0200

    [1.0] Кажется, кто-то сделал src плагина

---
+++ src/main/java/me/draimgoose/draimcoins/DCoins.java
@@ -0,0 +1,17 @@
+package me.draimgoose.draimcoins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DCoins extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
